f2cde76f5b654ff8db8a48eea1aa20f502e044aa
Inevitable52 <supersayjin5@hotmail.com>
2025-07-09 20:52:43 -0500

    Update percy.js

--- js/percy.js
+++ js/percy.js
@@ -1,6 +1,6 @@
 const logicMap = document.getElementById('logic-map');
 const seedsFolder = 'logic_seeds/';
-const seedRange = { start: 80, end: 123 };
+const seedRange = { start: 80, end: 124 };
 
 let seeds = {};
 
